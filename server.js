b3428d8bdfb730c4f9e55a0a04da1c2d4630e8d2
Tomas Hidalgo <tomashcordero@gmail.com>
2021-05-29 09:29:43 +1200

    It won't work

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build/')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use(bodyParser.urlencoded({
     extended: true
