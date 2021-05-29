0e02457c2d7da52bfb60232ebee87f3dfde67595
Tomas Hidalgo <tomashcordero@gmail.com>
2021-05-30 11:33:55 +1200

    Adding script for build

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -229,7 +229,7 @@ app.delete("/api/list/:userId", (req, res) => {
 });
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000, () => {
